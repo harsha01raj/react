8f100c3b14a3653af7a5a24eb618a5713b5de00f
Harsha <143447018+harsha01raj@users.noreply.github.com>
2023-11-01 21:17:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+};
+
+export default App;
